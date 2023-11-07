8b38c1fb957b4f9116277c5dd6598beda09af3f8
Laurent Cimon <laurent@nilio.ca>
2023-11-07 15:08:47 -0500

    Add a few fortunes

--- fortune.lisp
+++ fortune.lisp
@@ -1,6 +1,13 @@
 (defvar fortunes
   #("Fortune favours the brave"
     "No gods, No masters"
+    "Who do you think you are?"
+    "Quelle chance!"
+    "A fake plastic watering can..."
+    "This page was made possible by Mark C. Lisp"
+    "Sponsored by your local pizza place"
+    "What's new pussycat?"
+    "(&#955;&#119909;.&#119909;&#x1D465;)(&#955;&#119909;.&#119909;&#x1D465;)"
     "If you gaze for long into an abyss, the abyss gazes also into you"))
 
 (defun get-fortune ()
