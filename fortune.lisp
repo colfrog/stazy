8641cd630f474b7c40d560db16c4df19b9d93173
Laurent Cimon <laurent@nilio.ca>
2024-07-09 18:36:58 +0000

    Make the site more accessible for phones

--- fortune.lisp
+++ fortune.lisp
@@ -1,10 +1,8 @@
 (defvar fortunes
   #("Fortune favours the brave"
-    "No gods, No masters"
     "Quelle chance!"
     "The One True God is &#9675;"
     "Larry Wall was right"
-    "Dennis Ritchie is coding for God now"
     "No man ever steps in the same river twice,<br />for it's not the same river and he's not the same man"
     "This page was made possible by Mark C. Lisp"
     "Sponsored by your local pizza place"
