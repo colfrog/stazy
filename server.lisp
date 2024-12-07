b0c5efa9b7dc39e82945d8092c35e2d868245070
Laurent Cimon <laurent@nilio.ca>
2024-12-07 15:43:08 -0500

    Handle modifying the db from the lisp interface
    Needs login support, can be put behind HTTP login too

--- server.lisp
+++ server.lisp
@@ -6,7 +6,8 @@
 
 (load "fortune.lisp")
 
-(defvar *db-path* (or (last (uiop:command-line-arguments)) "/home/laurent/tulip/db.sqlite"))
+(defparameter *username* "laurent")
+(defvar *db-path* "/home/laurent/src/stazy/db.sqlite")
 (defvar *db* (sqlite:connect *db-path*))
 
 (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4243))
@@ -58,7 +59,7 @@
 		    (:a :href
 			(concatenate
 			 'string
-			 "/post?title="
+			 "post?title="
 			 (quri:url-encode (car item)))
 			(:h5 (cl-who:str (car item))))
 		    (:small (cl-who:str (cadr item)))))))))
@@ -70,7 +71,7 @@
   (let ((markdown
 	  (sqlite:execute-single
 	   *db*
-	   "select markdown from home where username = 'laurent'")))
+	   "select markdown from home where username = ?" *username*)))
     (when markdown
       (with-layout (nil)
 	(:article
@@ -83,8 +84,8 @@
     (let* ((results
 	    (sqlite:execute-to-list
 	     *db*
-	     "select markdown, submitted || ' (UTC)' from posts where username = 'laurent' and title = ?"
-	     title))
+	     "select markdown, submitted || ' (UTC)' from posts where username = ? and title = ?"
+	     *username* title))
 	   (markdown (caar results))
 	   (submitted (cadar results)))
       (when results
@@ -107,4 +108,99 @@
 	  (subseq (hunchentoot:request-uri hunchentoot:*request*) 3))))
     (sqlite:execute-single
      *db*
-     "select image from images where username = 'laurent' and id = ?" image-name)))
+     "select image from images where username = ? and id = ?" *username* image-name)))
+
+(hunchentoot:define-easy-handler (admin-new-post-page :uri "/admin/new-post") ()
+  (setf (hunchentoot:content-type*) "text/html")
+  (with-layout ("New Post")
+    (:form :id "new-post-form" :method "post" :action "/admin/new"
+	   (:label "Title: ")
+	   (:input :type "text" :id "title-input" :name "title" :required)
+	   (:br)
+	   (:textarea :id "text-input" :rows "30" :cols "80" :name "text" :required)
+	   (:br)
+	   (:input :type "submit"))))
+
+(hunchentoot:define-easy-handler (new-post-function :uri "/admin/new")
+    ((title :request-type :POST)
+     (text :request-type :POST))
+  (when (and title text)
+    (sqlite:execute-non-query *db* "insert into posts (username, title, markdown) values (?, ?, ?)"
+			      *username* title text)
+    (hunchentoot:redirect (concatenate 'string "/post?title=" title))))
+
+(hunchentoot:define-easy-handler (admin-edit-post-page :uri "/admin/post") (title)
+  (let ((text
+	  (if title
+	      (sqlite:execute-single
+	       *db*
+	       "select markdown from posts where title = ?" title)
+	      (sqlite:execute-single
+	       *db*
+	       "select markdown from home where username = ?" *username*))))
+    (with-layout ("Edit Post")
+      (:form :id "edit-post-form" :method "post" :action "/admin/edit"
+	     (:input :type "hidden" :id "title-input" :name "title" :readonly "readonly" :value title)
+	     (:textarea :id "text-input" :rows "30" :cols "80" :name "text" (cl-who:str text))
+	     (:br)
+	     (:input :type "submit")))))
+
+(hunchentoot:define-easy-handler (admin-edit :uri "/admin/edit")
+    ((title :request-type :POST)
+     (text :request-type :POST))
+  (when (and title text)
+    (if (not (equal title ""))
+	(progn
+	  (sqlite:execute-non-query *db* "update posts set markdown = ? where username = ? and title = ?"
+				    text *username* title)
+	  (hunchentoot:redirect (concatenate 'string "/post?title=" (quri:url-encode title))))
+	(progn
+	  (if (= 0 (sqlite:execute-single *db* "select count(*) from home where username = ?" *username*))
+	      (sqlite:execute-non-query *db* "insert into home (username, markdown) values (?, ?)" *username* text)
+	      (sqlite:execute-non-query *db* "update home set markdown = ? where username = ?" text *username*))
+	  (hunchentoot:redirect "/")))))
+
+(hunchentoot:define-easy-handler (admin-image-page :uri "/admin/images") ()
+  (let ((image-ids (sqlite:execute-to-list *db* "select id from images where username = ?" *username*)))
+    (with-layout ("Images")
+      (:div
+       (:center
+	(:form
+	 :id "new-image-form" :method "post" :action "/admin/i" :enctype "multipart/form-data"
+	 (:label "ID:")
+	 (:input :id "image-id" :name "id" :type "text")
+	 (:br)
+	 (:input :id "image-file" :name "file" :type "file" :accept "image/png,image/jpeg")
+	 (:br)
+	 (:input :type "submit")))
+       (:article
+	:style "display:flex; flex-direction:row; flex-wrap:wrap;"
+	(dolist (id image-ids)
+	  (let ((link (concatenate 'string "/i/" (car id))))
+	    (cl-who:htm
+	     (:span
+	      :style "padding:24px"
+	      (:p (:b (cl-who:str (car id))))
+	      (:a :href link (:img :src link :style "max-width: 280px; max-height: 280px;"))
+	      (:form
+	       :id (concatenate 'string "delete-" (car id) "-form")
+	       :method "post"
+	       :action "/admin/i/delete"
+	       (:input :name "id" :value (car id) :type "hidden")
+	       (:input :type "submit" :value "x")))))))))))
+
+(hunchentoot:define-easy-handler (admin-image-upload :uri "/admin/i")
+    ((id :request-type :POST)
+     (file :request-type :POST))
+  (when (and id file)
+    (with-open-file (stream (car file) :element-type 'unsigned-byte)
+      (let ((buffer (make-array (file-length stream) :initial-element nil)))
+	(read-sequence buffer stream)
+	(sqlite:execute-non-query *db* "insert into images (username, id, image) values (?, ?, ?)" *username* id buffer)))
+    (hunchentoot:redirect "/admin/images")))
+
+(hunchentoot:define-easy-handler (admin-image-delete :uri "/admin/i/delete")
+    ((id :request-type :POST))
+  (when id
+    (sqlite:execute-non-query *db* "delete from images where username = ? and id = ?" *username* id)
+    (hunchentoot:redirect "/admin/images")))))
